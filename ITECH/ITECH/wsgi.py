0b18445ef8728adc73ced5bbb993d03a70f6de65
Sennorin <dushichuan001@outlook.com>
2025-03-09 03:51:42 +0000

    Update wsgi.py

--- ITECH/ITECH/wsgi.py
+++ ITECH/ITECH/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+project_path = "/home/Sennori/yourproject"
+sys.path.append(project_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ITECH.settings')
 
 application = get_wsgi_application()
