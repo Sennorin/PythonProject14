30891c480fa5cb6b62111ee7ef684f468dab7829
Shichuan <2924361d@student.gla.ac.uk>
2025-03-09 19:06:07 +0000

    更新了首页样式

--- ITECH/ITECH/settings.py
+++ ITECH/ITECH/settings.py
@@ -26,9 +26,7 @@ AUTH_USER_MODEL = 'userprofile.UserProfile'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["Sennori.pythonanywhere.com", 'sennori1.eu.pythonanywhere.com']
-
-
+ALLOWED_HOSTS = ["Sennori.pythonanywhere.com", 'sennori1.eu.pythonanywhere.com','*']
 
 
 # Application definition
--- ITECH/ITECH/wsgi.py
+++ ITECH/ITECH/wsgi.py
@@ -7,6 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+
 import os
 
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ITECH.settings')
+
 application = get_wsgi_application()
+
