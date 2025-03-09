35c519806f02d32b6e0fae2af3ee2f8d996566e2
Sennorin <dushichuan001@outlook.com>
2025-03-09 04:33:49 +0000

    Update settings.py

--- ITECH/ITECH/settings.py
+++ ITECH/ITECH/settings.py
@@ -26,7 +26,8 @@ AUTH_USER_MODEL = 'userprofile.UserProfile'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["Sennori.pythonanywhere.com"]
+ALLOWED_HOSTS = ["Sennori.pythonanywhere.com", 'sennori1.eu.pythonanywhere.com']
+
 
 
 
